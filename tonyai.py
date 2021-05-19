107c602a76c50c67dc8b12e598362c1402658089
Kaushal Kumar <65879896+iamkaushalkb@users.noreply.github.com>
2021-05-19 20:13:34 +0545

    [ADD] speechai.py : Files added

---
+++ tonyai.py
@@ -0,0 +1,94 @@
+# importing modules
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+# speech_voice
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+# running audio
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+# Wishing User According to the time
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Tony Sir. Please tell me how may I help you")       
+
+# Creating Function to take command
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-us')
+        print(f"User said: {query}\n")
+
+    except Exception as e:  
+        print("Say that again please...")  
+        return "None"
+    return query
+
+# Creating Function for sending email
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+        
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+        elif 'open pictures' in query:
+            pic_dir = 'C://Users//uSer//Pictures'
+            pic = os.listdir(pic_dir)
+            print(pic)    
+            os.startfile(os.path.join(pic_dir, pic[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M")    
+            speak(f"Sir, the time is {strTime}")
+
